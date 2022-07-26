f01389dee4b975eb02b24a60bb8ba10610801f36
Malte Tammena <malte.tammena@pm.me>
2022-07-26 20:34:40 +0200

    Basic app, re-README

--- src/app.rs
+++ src/app.rs
@@ -1,12 +1,34 @@
 use yew::prelude::*;
 
-#[function_component(App)]
-pub fn app() -> Html {
-    html! {
-        <main>
-            <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
-            <h1 class="text-red-500 text-4xl">{ "Hello World!" }</h1>
-            <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
-        </main>
+pub struct App {
+    nothing: u8,
+}
+
+pub enum Msg {
+
+}
+
+impl Component for App {
+    type Message = Msg;
+
+    type Properties = ();
+
+    fn create(ctx: &Context<Self>) -> Self {
+        Self {
+            nothing: 0,
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        html! {
+            <main class="w-screen h-screen flex flex-col">
+                <p class="text-4xl text-center">{ "this will have stuff soon" }</p>
+                <div class="flex flex-row grow h-full">
+                    <p class="grow shrink">{ "the game?" }</p>
+                    <p class="w-40">{ "chat?" }</p>
+                </div>
+                <p class="text-center">{ "cards?" }</p>
+            </main>
+        }
     }
 }
